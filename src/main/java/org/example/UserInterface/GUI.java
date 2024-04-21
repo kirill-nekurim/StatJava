ad725c82ad8e06e5aed9f6b41f92fd79990b5882
kirill-nekurim <147753108+kirill-nekurim@users.noreply.github.com>
2024-04-21 18:32:21 +0300

    Add gui

--- src/main/java/org/example/UserInterface/GUI.java
+++ src/main/java/org/example/UserInterface/GUI.java
@@ -1,4 +1,11 @@
 package org.example.UserInterface;
 
+import javax.swing.*;
+
 public class GUI {
+
+    private JTextField textField1;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
 }
