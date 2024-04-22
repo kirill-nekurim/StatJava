ef4d6da9809049bd4c78352dd54b4f2575823775
kirill-nekurim <147753108+kirill-nekurim@users.noreply.github.com>
2024-04-22 21:18:20 +0300

    Оно как-то работает

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import com.formdev.flatlaf.themes.FlatMacDarkLaf;
 import com.formdev.flatlaf.themes.FlatMacLightLaf;
 import java.io.IOException;
 import javax.swing.UIManager;
@@ -8,8 +9,8 @@ import org.example.UserInterface.GUI;
 public class Main {
     public static void main(String[] args) throws IOException{
         try {
-            //UIManager.setLookAndFeel(new FlatMacDarkLaf());
-            UIManager.setLookAndFeel(new FlatMacLightLaf());
+            UIManager.setLookAndFeel(new FlatMacDarkLaf());
+            //UIManager.setLookAndFeel(new FlatMacLightLaf());
         } catch (UnsupportedLookAndFeelException e) {
             throw new RuntimeException(e);
         }
--- src/main/java/org/example/Operation/ReaderXLS.java
+++ src/main/java/org/example/Operation/ReaderXLS.java
@@ -6,7 +6,6 @@ import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
-import org.example.Main;
 
 public class ReaderXLS {
 
@@ -18,14 +17,13 @@ public class ReaderXLS {
             System.out.println("Ошибка при чтении Excel файла: " + e.getMessage());
             return null;
         }
-
         Sheet sheet;
         if (a) {
             try {
                 int index = Integer.parseInt(which) - 1;
                 sheet = workbook.getSheetAt(index);
             } catch (IllegalArgumentException e) {
-                System.out.println("Неверный формат листа. Используется первый лист.");
+                System.out.println("Неверный формат индекса листа. Используется первый лист.");
                 sheet = workbook.getSheetAt(0);
             }
         } else {
@@ -33,7 +31,7 @@ public class ReaderXLS {
         }
 
         if (sheet == null) {
-            System.out.println("Лист не найден, используйте первый лист.");
+            System.out.println("Лист не найден, используется первый лист.");
             sheet = workbook.getSheetAt(0);
         }
 
@@ -54,52 +52,55 @@ public class ReaderXLS {
                 }
             }
         }
+
         workbook.close();
         return data;
     }
 
 public void writeXLSX(double[][] mas) throws IOException{
-    XSSFWorkbook book = new XSSFWorkbook();
-    XSSFSheet sheet = book.createSheet("Полученные значения");
-    String[] StatNames = {"Среднее геометрическое", "Среднее арифметическое", "Оценка стандартного отклонения", "Размах", "Коэффициент ковариации с последующей выборкой", "Количество элементов",
+    Workbook workbook = new XSSFWorkbook();
+    Sheet mainSheet = workbook.createSheet("Полученные значения");
+    Sheet covarianceSheet = workbook.createSheet("Матрица ковариации");
+
+    String[] statNames = {"Среднее геометрическое", "Среднее арифметическое", "Оценка стандартного отклонения", "Размах", "Коэффициент ковариации с последующей выборкой", "Количество элементов",
             "Нижняя граница доверительного интервала", "Верхняя граница доверительного интервала", "Оценка дисперсии", "Максимум", "Минимум"};
-    for (int i = 0; i < 11; i++) {
-        Row row = sheet.createRow(i);
-        int cellNumber = 0;
-        for (int j = 0; j < Repository.getInstance().getMatrix().length; j++) {
-            Cell name = row.createCell(cellNumber);
-            name.setCellValue(StatNames[i] + " для " + (j + 1) + "-й выборки: ");
-            Cell x = row.createCell(cellNumber + 1);
-            x.setCellValue((Repository.getInstance().getParameters())[i][j]);
-            cellNumber += 2;
+
+    for (int i = 0; i < statNames.length; i++) {
+        Row row = mainSheet.createRow(i);
+        for (int j = 0; j < mas.length; j++) {
+            Cell nameCell = row.createCell(j * 2);
+            nameCell.setCellValue(statNames[i] + " для " + (j + 1) + "-й выборки: ");
+
+            Cell valueCell = row.createCell(j * 2 + 1);
+            valueCell.setCellValue((Repository.getInstance().getParameters())[i][j]);
         }
-        sheet.autoSizeColumn(i);
     }
-    XSSFSheet sheet2 = book.createSheet("Матрица ковариации");
-    Row row = sheet2.createRow(0);
-    for (int j = 0; j < Repository.getInstance().getMatrix().length; j++) {
-        Cell name = row.createCell(j + 1);
-        name.setCellValue("Выборка " + (j + 1));
-    }
-    for (int j = 1; j <= Repository.getInstance().getMatrix().length; j++) {
-        row = sheet2.createRow(j);
-        Cell name = row.createCell(0);
-        name.setCellValue("Выборка " + j);
-        for (int i = 1; i <= Repository.getInstance().getMatrix().length; i++) {
-            name = row.createCell(i);
-            name.setCellValue(Repository.getInstance().getCov(i - 1, j - 1));
+
+    for (int i = 0; i < mas.length; i++) {
+        Row headerRow = covarianceSheet.createRow(0);
+        headerRow.createCell(i + 1).setCellValue("Выборка " + (i + 1));
+
+        for (int j = 0; j < mas.length; j++) {
+            if (i == 0) {
+                Row sampleRow = covarianceSheet.createRow(j + 1);
+                sampleRow.createCell(0).setCellValue("Выборка " + (j + 1));
+            }
+            covarianceSheet.getRow(j + 1).createCell(i + 1).setCellValue(Repository.getInstance().getCov(i, j));
         }
-        sheet2.autoSizeColumn(j - 1);
     }
-    try {
-        book.write(new FileOutputStream("test.xlsx"));
-        System.out.println("Parameters were exported successfully");
-    } catch (FileNotFoundException e) {
-        System.out.println("Close file firstsly");
+
+    try (FileOutputStream fileOut = new FileOutputStream("test.xlsx")) {
+        workbook.write(fileOut);
+        System.out.println("Параметры успешно экспортированы");
+    } catch (IOException e) {
+        System.out.println("Ошибка экспорта параметров: " + e.getMessage());
+    } finally {
+        workbook.close();
     }
-    book.close();
 }
 }
+
+
 //        Workbook workbook = new XSSFWorkbook();
 //        Sheet mainSheet = workbook.createSheet("Полученные значения");
 //        Sheet covarianceSheet = workbook.createSheet("Матрица ковариации");
